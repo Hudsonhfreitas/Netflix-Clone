c26e5ef1031438e27df3577418802f3932ca944f
Hudson Henrique de Freitas <hudson@MINERACAOCAFE.local>
2021-09-02 13:44:09 -0300

    update

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,5 +1,7 @@
 export default function MovieInfo({info, setShowInfo}) {
 
+    let date = new Date(info.first_air_date);
+
     function handleClose(e) {
         var target = e.target.className
         if(target.includes('close')) {
@@ -13,9 +15,13 @@ export default function MovieInfo({info, setShowInfo}) {
                 <button className="close"><i className="fas fa-times close"></i></button>
                 <img src={`https://image.tmdb.org/t/p/w500${info.backdrop_path}`} alt={info.name}></img>
                 <div className="info">
-                    <h2>{info.name}</h2>
-                    <p>{info.overview}</p>
-
+                    <div className="top">
+                        <h2>{info.name}</h2>
+                        <span className="points">{info.vote_average} pontos</span>
+                        <span className= "date">{date.getFullYear()}</span>
+                        <span className="country">{info.origin_country[0]}</span>
+                        <p>{info.overview}</p>                       
+                    </div>
                     <div className="buttons">
                             <button className="watch"><i className="fas fa-play"></i>Assistir</button>
                             <button className="myList"><i className="fas fa-plus"></i>Minha Lista</button>
